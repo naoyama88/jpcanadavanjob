6ac4b9b968a6f0495f7aa3407178afe7c3261ce0
naoyama88 <naoyama88@gmail.com>
2019-11-07 14:58:07 -0800

    comment out test code

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -12,11 +12,11 @@ class CommandController
     public function test()
     {
         // echo "test!";
-        $twitterService = new TwitterService();
-        $cKey = env('TWITTER_CONSUMER_KEY', 'error');
-        $cSecret = env('TWITTER_CONSUMER_SECRET', 'error');
-        $aToken = env('TWITTER_ACCESS_TOKEN', 'error');
-        $aTokenSecret = env('TWITTER_ACCESS_TOKEN_SECRET', 'error');
-        $twitterService->tweet("test", $cKey, $cSecret, $aToken, $aTokenSecret);
+        // $twitterService = new TwitterService();
+        // $cKey = env('TWITTER_CONSUMER_KEY', 'error');
+        // $cSecret = env('TWITTER_CONSUMER_SECRET', 'error');
+        // $aToken = env('TWITTER_ACCESS_TOKEN', 'error');
+        // $aTokenSecret = env('TWITTER_ACCESS_TOKEN_SECRET', 'error');
+        // $twitterService->tweet("test", $cKey, $cSecret, $aToken, $aTokenSecret);
     }
 }
